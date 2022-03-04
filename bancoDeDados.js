5bd613b8b8ec92f5f4e65bb473597be637f79357
Giani R Pertuzatti <giani.pertuzatti.pb@compasso.com.br>
2022-03-04 15:16:33 -0300

    Atualização cardápio

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -1,10 +1,14 @@
-const bancoDeDados = [{
-    "nomePizza": "Pizza de Calabresa",
-    "valorPizza": 40
-}, {
-    "nomePizza": "Pizza de Frango",
-    "valorPizza": 40
-}
+const bancoDeDados = [
+    {
+        "nomePizza": "Pizza de Calabresa",
+        "valorPizza": 40
+    }, {
+        "nomePizza": "Pizza de Mussarela",
+        "valorPizza": 45
+    }, {
+        "nomePizza": "Pizza de Portuguesa",
+        "valorPizza": 40
+    }
 ]
 
 module.exports = bancoDeDados
